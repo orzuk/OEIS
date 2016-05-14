07f866d57e9992c81899114be72c7eda727fa4b5
U-DESKTOP-ARD4MPG\oorzu <or.zuk@mail.huji.ac.il>
2016-05-15 01:01:20 +0300

    added filtering

--- src/filter_seqs.py
+++ src/filter_seqs.py
@@ -1,3 +1,14 @@
 # Filter seuqnces which are too short 
-def filter_seqs(seqs, names):
+import numpy as np 
 
+
+def filter_seqs(seqs):
+
+	seqs = np.array(seqs[4:-1]) # remove header and convert to numpy array 
+	num_seqs = len(seqs)
+	filter_vec = [1] * num_seqs
+	for i in range(0, num_seqs): # loop on 
+		if len(seqs[i]) < 5: # remove short sequences
+			filter_vec[i] = 0 	
+	seqs_filtered = seqs[np.nonzero(filter_vec)]
+	return seqs_filtered
--- src/read_OEIS.py
+++ src/read_OEIS.py
@@ -1,5 +1,8 @@
 # Read OEIS data 
 
+import numpy as np 
+import filter_seqs as fs
+
 seq_file = open("C:/Users/oorzu/Documents/GitHub/OEIS/data/stripped", "r")
 names_file = open("C:/Users/oorzu/Documents/GitHub/OEIS/data/names", "r")
 
@@ -18,9 +21,29 @@ for i in range(0,len(seqs)):
         seq_lens[i] = len(seqs[i])
 names_file.close()
 
+# Print example 
 print names[4]
 print seqs[4]
 
 seq_file.close()
 names_file.close()
+
+
+# Filter sequences 
+filtered_seqs = fs.filter_seqs(seqs)
+
+print "Num. filtered="+str(len(filtered_seqs)) # print seqs
+
+
+# Cluster sequences
+
+
+
+# Visualize sequences 
+
+
+
+
+# Predict missing values 
+
 
