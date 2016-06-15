5e21a9a585aa08abd03f6ce27be7f997452d8dfa
ayelet <ayelet.blass@mail.huji.ac.il>
2016-06-15 21:14:08 +0300

    added images and function

---
+++ Filtering and summary statistics/getPercentageSquared.m
@@ -0,0 +1,17 @@
+function [ counts ] = getPercentageSquared( mat,epsilon )
+%UNTITLED6 Summary of this function goes here
+%   Detailed explanation goes here
+
+lengths = size(mat,2)-sum(isnan(mat),2)-sum((mat<0),2);
+mat(isnan(mat))=3;
+mat(mat<0)=3;
+p=(mod(sqrt(mat), 1))<epsilon;
+percentages = sum(p,2)./lengths*100;
+h = histogram(percentages,(0:1:100));
+title('percentage of squared in series');
+xlabel('percentage of squared');
+counts = h.Values;
+saveas(gcf,'SquaredPercent.png');
+
+
+end
