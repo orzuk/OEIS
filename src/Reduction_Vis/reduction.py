87e740a6bfea3efb4365b461f5d4c52f94a9d794
liorfox <lior.fox@gmail.com>
2016-06-26 18:33:42 +0300

    add support for labels, generalizing plotting

--- src/Reduction_Vis/reduction.py
+++ src/Reduction_Vis/reduction.py
@@ -2,6 +2,7 @@
 import numpy as np
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 # add to python environment the directory above the one the file is in (src)
 import sys
 import os
@@ -16,28 +17,45 @@ def main():
 	print(len(features[0].dtype.descr))
 	general_field_names = ["name", "length"]
 	feature_names = [name for (name, typ) in features[0].dtype.descr if name not in general_field_names]
-	names = ftr.extract_features(features, ["name"])
+	feature_names = ["var","mean"]
+
+	#names = ftr.extract_features(features, ["name"])
+	names = features[["name"]]
 	X = ftr.extract_features(features, feature_names)
+	labels = np.asarray([i%100 for i in range(X.shape[0])])
 
 	# print(features[0].dtype)
 	# print(feature_names)
 	#print(features[0][1])
 	test_pca(names, X)
+	
 
-def test_pca(names, X):
+def test_pca(names, X, labels=None):
 	"""runs a PCA analysis on features"""
 
-	print(X.shape)
-
 	X = X[np.isfinite(X).all(axis=1)]
-	print (X.shape)
 	pca = PCA(n_components=2)
 	#pca.fit(X)
 	X_pc = pca.fit_transform(X)
+	plt_scatter(names, X_pc, labels)
+
+
+	print(pca.explained_variance_ratio_)
+
+def plt_scatter(names,X, labels=None):
+	if labels is None:
+		labels = np.asarray([0 for i in range(X.shape[0])])
+
+	cls = set(labels)
+
+	colors = cm.rainbow(np.linspace(0, 1, len(cls)))
 	plt.figure()
-	plt.scatter(X_pc[:,0], X_pc[:,1])
+	for c,l in zip(cls,colors):
+		idx = labels == c
+		plt.scatter(X[idx,0], X[idx,1],c = l)
+
 	plt.show()
-	#print(pca.explained_variance_ratio_)
+
 
 if __name__ == '__main__':
 	main()
