aa5d7204169b3e77021faf425d9c894de0afa4a3
aymb <ayeletblass@gmail.com>
2016-07-07 11:53:43 +0300

    fixed convergence function and added the changed resulting figure  the commit message for your changes. Lines starting

--- Filtering and summary statistics/getConverges.m
+++ Filtering and summary statistics/getConverges.m
@@ -1,18 +1,23 @@
-function [ counts ] = getConverges( mat )
+
 %getConverges 
 %   checks whether sequence converges.
 %a sequence is defines as convergin if the last
 % 10 members in the sequence fulfill the ratio test
-nan = isnan(mat);
+load cleaned_data;
+nan = isnan(cleaned_data);
 g= ones(size(nan,1),1);
 nan=[nan g];
 v=[1:size(nan,1)].';
 B=accumarray(v,v,[],@(i) find(nan(i,:)==1,1,'first'));
-last10 = abs(mat(:,B-10:B-1));
-converges = sum(( sum((last10(:,2:10)./last10(:,1:9))<1,2)==9 ));
-mightConverge = sum(( sum((last10(:,2:10)./last10(:,1:9))<=1,2)==9 ));
+last10 = abs(cleaned_data(:,B-10:B-1));
+div=last10(:,2:10)./last10(:,1:9);
+monodec=sum(all(diff(div,2)<0,2));
+mightmonodec=sum(all(diff(div,2)<=0,2));
 
-bar([converges,mightConverge-converges,size(mat,1)-mightConverge]/size(mat,1));
+%converges = sum(( sum((last10(:,2:10)./last10(:,1:9))<1,2)==9 ));
+%mightConverge = sum(( sum((last10(:,2:10)./last10(:,1:9))<=1,2)==9 ));
+
+bar([monodec,mightmonodec-monodec,size(cleaned_data,1)-mightmonodec]/size(cleaned_data,1));
 set(gca,'XTickLabel',{'converges', 'inconclusive', 'diverges'})
 
 
@@ -21,5 +26,4 @@ title('percentage of convergent  series');
 saveas(gcf,'convergence.png');
 
 
-end
 
