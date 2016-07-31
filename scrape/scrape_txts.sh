e47fe65ee6d2cb8d4cf74c6f773d6fae491a767a
Dimid Duchovny <dimidd@gmail.com>
2016-07-31 09:35:17 +0300

    txt: add default ids file
    Signed-off-by: Dimid Duchovny <dimidd@gmail.com>

--- scrape/scrape_txts.sh
+++ scrape/scrape_txts.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 IDS_FILE="$1"
+: ${IDS_FILE:='data/ids'}
 MAX_IDS=5
 head "-${MAX_IDS}" "${IDS_FILE}" | xargs -L1 -I{} bash -c "curl 'http://oeis.org/search?q=id:{}&fmt=text' > scrape/txts/{}.txt"
