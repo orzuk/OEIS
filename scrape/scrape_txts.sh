76bea251b239410c750101d427913a405dfbb562
Dimid Duchovny <dimidd@gmail.com>
2016-07-31 10:24:57 +0300

    txt: refactor url
    Signed-off-by: Dimid Duchovny <dimidd@gmail.com>

--- scrape/scrape_txts.sh
+++ scrape/scrape_txts.sh
@@ -3,4 +3,5 @@ IDS_FILE="$1"
 : ${IDS_FILE:='data/ids'}
 MAX_IDS=5
 TXTS_DIR='scrape/txts'
-head "-${MAX_IDS}" "${IDS_FILE}" | xargs -L1 -I{} bash -c "[[ -f ${TXTS_DIR}/{}.txt ]] || curl 'http://oeis.org/search?q=id:{}&fmt=text' > ${TXTS_DIR}/{}.txt"
+URL='http://oeis.org/search?q=id:'
+head "-${MAX_IDS}" "${IDS_FILE}" | xargs -L1 -I{} bash -c "[[ -f ${TXTS_DIR}/{}.txt ]] || curl '${URL}{}&fmt=text' > ${TXTS_DIR}/{}.txt"
