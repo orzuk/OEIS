84fce34369cc136c8c6db5b4f3a21abf944b3b23
Dimid Duchovny <dimidd@gmail.com>
2016-07-31 09:53:43 +0300

    txt: make download idempotent
    Signed-off-by: Dimid Duchovny <dimidd@gmail.com>

--- scrape/scrape_txts.sh
+++ scrape/scrape_txts.sh
@@ -2,4 +2,5 @@
 IDS_FILE="$1"
 : ${IDS_FILE:='data/ids'}
 MAX_IDS=5
-head "-${MAX_IDS}" "${IDS_FILE}" | xargs -L1 -I{} bash -c "curl 'http://oeis.org/search?q=id:{}&fmt=text' > scrape/txts/{}.txt"
+TXTS_DIR='scrape/txts'
+head "-${MAX_IDS}" "${IDS_FILE}" | xargs -L1 -I{} bash -c "[[ -f ${TXTS_DIR}/{}.txt ]] || curl 'http://oeis.org/search?q=id:{}&fmt=text' > ${TXTS_DIR}/{}.txt"
