806dacc38058ae564e1128930f4f414aec589734
aymb <ayeletblass@gmail.com>
2016-08-15 16:03:09 +0300

    moved to correct file

--- Filtering and summary statistics/hist_appear.m
+++ Filtering and summary statistics/hist_appear.m
@@ -2,17 +2,12 @@
 % in each sequence and computed the median
 % of the occurrences
 
-%dataCat: 1 - for cleaned data (compute for all the sequences in the cleaned data) and choose vecSeq=[]; 
-%0 - specific sequences from the uncleaned data (in this
-%case you have to add a vector 'vecSeq' that correspond to the IDs of the different
-%sequences you would like to make the computation. ex: seqID=(3,100,5000) represent the sequences (A000003,A000100,A005000)
+function hist_appear(seqID)
 
-function hist_appear(dataCat,vecSeq)
-
-if dataCat==1
-   load('cleaned_data.mat')
+if nargin == 1
+	cleaned_data=getSeq(seqID);
 else
-   cleaned_data=getSeq(vecSeq);
+	load cleaned_data;
 end
 
 Ncount_vec=zeros(1,size(cleaned_data,1));
