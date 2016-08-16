959290d8926f7a3de0eb874db44047a78dd85fbb
yowald <yoav.wald@gmail.com>
2016-08-16 16:11:07 +0300

    filter sequences for which we have less than 30 numbers

--- prediction/oeis_filter.py
+++ prediction/oeis_filter.py
@@ -1,1 +1,12 @@
 """ TODO: nothing here yet """
+import numpy as np
+
+# filters sequences where we do not know or have the first 30 numbers
+def filter_short_seqs(dataset):
+    good_samples = np.array([],dtype="int32")
+    for i in range(len(dataset)):
+        if(np.all(dataset[i,:,:]<255)):
+            good_samples = np.append(good_samples,[int(i)])
+    # print(good_samples[7])
+    # print(dataset[good_samples[7],:,:])
+    return dataset[good_samples,:,:]
